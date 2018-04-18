1ba7b34cfe9dbdc267915fc07fefd300c24c9217
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-04-18 09:21:52 +0100

    Drafting v3

--- Makefile
+++ Makefile
@@ -1,8 +1,3 @@
-SCALA_VERSION?= 2.12
-KAFKA_VERSION?= 1.1.0
-KAFKA_DIR= kafka_$(SCALA_VERSION)-$(KAFKA_VERSION)
-KAFKA_SRC= https://archive.apache.org/dist/kafka/$(KAFKA_VERSION)/$(KAFKA_DIR).tgz
-KAFKA_ROOT= testdata/$(KAFKA_DIR)
 PKG=$(shell go list ./... | grep -v vendor)
 
 default: vet test
@@ -10,26 +5,30 @@ default: vet test
 vet:
 	go vet $(PKG)
 
-test: testdeps
-	KAFKA_DIR=$(KAFKA_DIR) go test $(PKG) -ginkgo.slowSpecThreshold=60
+test:
+	go test $(PKG) -ginkgo.slowSpecThreshold=60
 
-test-verbose: testdeps
-	KAFKA_DIR=$(KAFKA_DIR) go test $(PKG) -ginkgo.slowSpecThreshold=60 -v
+test-verbose:
+	go test $(PKG) -ginkgo.slowSpecThreshold=60 -v
 
-test-race: testdeps
-	KAFKA_DIR=$(KAFKA_DIR) go test $(PKG) -ginkgo.slowSpecThreshold=60 -v -race
+test-race:
+	go test $(PKG) -ginkgo.slowSpecThreshold=60 -v -race
 
-testdeps: $(KAFKA_ROOT)
+.PHONY: vet test test-race test-verbose
+
+scenario.up:
+	docker-compose -f testdata/docker-compose.yml up
+
+scenario.rm:
+	docker-compose -f testdata/docker-compose.yml rm -f
+
+.PHONY: scenario.up scenario.rm
 
 doc: README.md
 
-.PHONY: test testdeps vet doc
+.PHONY: doc
 
 # ---------------------------------------------------------------------
-
-$(KAFKA_ROOT):
-	@mkdir -p $(dir $@)
-	cd $(dir $@) && curl -sSL $(KAFKA_SRC) | tar xz
 
 README.md: README.md.tpl $(wildcard *.go)
 	becca -package $(subst $(GOPATH)/src/,,$(PWD))
---
+++ example_test.go
@@ -0,0 +1,62 @@
+package cluster_test
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/Shopify/sarama"
+	cluster "github.com/bsm/sarama-cluster"
+)
+
+func Example() {
+	config := sarama.NewConfig()
+	config.ClientID = "my-client"
+	config.Version = sarama.V0_10_2_0
+	config.Consumer.Return.Errors = true
+
+	// define a (thread-safe) handler
+	handler := cluster.HandlerFunc(func(pc cluster.PartitionConsumer) error {
+		for {
+			select {
+			case msg, more := <-pc.Messages():
+				if !more {
+					return nil
+				}
+				fmt.Fprintf(os.Stdout, "%s-%d:%d\t%s\t%s\n", msg.Topic, msg.Partition, msg.Offset, msg.Key, msg.Value)
+				pc.MarkMessage(msg, "") // mark message as processed
+			case <-pc.Done():
+				return nil
+			}
+		}
+	})
+
+	// init consumer
+	brokers := []string{"127.0.0.1:9092"}
+	topics := []string{"my_topic", "other_topic"}
+	consumer, err := cluster.NewConsumer(brokers, "my-consumer-group", topics, config, handler)
+	if err != nil {
+		panic(err)
+	}
+	defer consumer.Close()
+
+	// consume errors
+	go func() {
+		for err := range consumer.Errors() {
+			log.Printf("Error: %v\n", err)
+		}
+	}()
+
+	// consume claims
+	go func() {
+		for claim := range consumer.Claims() {
+			log.Printf("Claimed: %+v\n", claim.Topics)
+		}
+	}()
+
+	// trap SIGINT to trigger a shutdown.
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
+	<-signals
+}
