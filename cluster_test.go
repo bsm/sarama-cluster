9aa81a2ec8ce5bd057d733286d77628d5a2ae9c9
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2018-08-16 16:22:29 +0100

    Revert "Fix tests"
    This reverts commit abb0e9f5dd57b5a3167e1472136a84c4b989478b.

--- cluster_test.go
+++ cluster_test.go
@@ -198,4 +198,4 @@ func (*mockConsumer) ConsumePartition(topic string, partition int32, offset int6
 	}, nil
 }
 
-func (*mockPartitionConsumer) AsyncClose() {}
+func (*mockPartitionConsumer) Close() error { return nil }
