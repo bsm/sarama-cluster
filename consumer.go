f7d869db45fd8d74bf62c02a9d17637f945668d0
Steve van Loben Sels <steve.vanlobensels@gmail.com>
2018-09-18 05:35:12 -0700

    made error handling in PartitionModeConsumer conform to docs (#267)

--- consumer.go
+++ consumer.go
@@ -824,7 +824,7 @@ func (c *Consumer) createConsumer(tomb *loopTomb, topic string, partition int32,
 	// Start partition consumer goroutine
 	tomb.Go(func(stopper <-chan none) {
 		if c.client.config.Group.Mode == ConsumerModePartitions {
-			pc.waitFor(stopper, c.errors)
+			pc.waitFor(stopper)
 		} else {
 			pc.multiplex(stopper, c.messages, c.errors)
 		}
